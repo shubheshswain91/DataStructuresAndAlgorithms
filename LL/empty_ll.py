0cfaefa44e6c95ab8b3281dc7cdc7f1781b205e7
shubheshswain91 <shubheshswain91@gmail.com>
2024-05-29 11:10:54 +0200

    Node Traversal function

--- LL/empty_ll.py
+++ LL/empty_ll.py
@@ -23,9 +23,37 @@ class LinkedList:
 
     def __len__(self):
         return self.n 
-        
 
+    def insert_head(self, value):
+
+        # new node
+        new_node = Node(value)
+
+        # create connection
+        new_node.next = self.head 
+
+        # reassign head
+        self.head = new_node
+
+        # increment n
+        self.n += 1
+
+    def __str__(self):
+        curr = self.head 
+        result = ''
+        while curr != None:
+            #print(curr.value)
+            result = result + str(curr.value) + '->'
+            curr = curr.next
+        return result    
 
 L = LinkedList()       
-print(L) 
-print(len(L))      
+
+
+L.insert_head(1)
+L.insert_head(2)
+L.insert_head(3)
+L.insert_head(4)
+#L.traverse()
+print(L)
+#print(len(L))
